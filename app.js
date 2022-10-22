8b2ee212fa68aa69981ebfca067044e08fa671e5
valentinchavez <valeonchavez@gmail.com>
2022-10-22 18:17:52 -0300

    borrando

--- app.js
+++ app.js
@@ -114,25 +114,3 @@ const actualizarCarrito = () => {
     //empezando en 0.
 
 }
-
-fetch("products.json")
-    .then(function(response){
-        return response.json();
-})
-.then(function(products){
-    let placeholder = document.querySelector("#data-output");
-    let out = "";
-    for(let product of products){
-        out += `
-            <tr>
-                <td> <img src='${product.image}'> </td>
-                <td>${product.name}</td>
-                <td>${product.price}</td>
-                <td>${product.inventory}</td>
-            </tr>
-        `;
-    }
-
-    placeholder.innerHTML = out;
-});
-    
